ceccf9bb60486fd62b74e773e5635c77df92a7ec
agutierrez72 <agutierrez72@ucmerced.edu>
2020-02-21 15:01:45 -0800

    add  & test functionality of search input & button

--- static/main.js
+++ static/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 	// The entire database should be displayed when your app starts
 	// The Search button should be programmed to hit the /search endpoint along with the query string from the text box
+
+	$("#search").click(function(){
+		var find = $("#animal").val();
+
+		var message ="Find " + find;
+		alert(message);
+	});
 });
